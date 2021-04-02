8a0b84fd8a7c90524be815358229825e473620b3
Martin Renteria <renteriamp96@gmail.com>
2021-04-01 23:47:51 -0700

    Added Google project ot Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Burger from '../Portfolio/Projects/Burger/Burger-Muncher';
+import Google from '../Portfolio/Projects/Google-React-Search/Google'
 
 function Portfolio() {
     return (
-      <Burger />
+      <Burger />,
+      <Google />
     )
 }
 
