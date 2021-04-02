b085fca4ee67740b2c570bb8f4e70f562ac399e8
Martin Renteria <renteriamp96@gmail.com>
2021-04-01 23:17:46 -0700

    Added image to project card on portfolio

--- src/components/Portfolio/Projects/Burger-Muncher.js
+++ src/components/Portfolio/Projects/Burger-Muncher.js
@@ -1,20 +1,19 @@
-/* eslint-disable react/jsx-no-target-blank */
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import BurgerApp from './burger-app.png';
 
 function Burger() {
     return (
 <Card style={{ width: '25rem' }} bg="dark" text="white" className="text-center">
-  <Card.Img variant="top" alt="Screenshot"/>
+  <Card.Img variant="top" src={BurgerApp} alt="Screenshot"/>
   <Card.Body>
     <Card.Title>Burger Muncher</Card.Title>
     <Card.Text>
     This application required the knowledge of modals, using object relational mapping(ORM), MySQL, Node and handlebars in order to collect the users input display their desired food and devour it!
     </Card.Text>
-    <Button className="projectButtons" variant="dark"><a href="https://burger-app-node-handlebars.herokuapp.com/burgers" target="_blank">Live Site</a></Button>
-    <Button className="projectButtons" variant="dark"><a href="https://github.com/MartinRenteria/Burger-Logger" target="_blank">Github Repo</a></Button>
+    <Button className="projectButtons" variant="dark"><a href="https://burger-app-node-handlebars.herokuapp.com/burgers" target="_blank" rel="noreferrer">Live Site</a></Button>
+    <Button className="projectButtons" variant="dark"><a href="https://github.com/MartinRenteria/Burger-Logger" target="_blank" rel="noreferrer">Github Repo</a></Button>
   </Card.Body>
 </Card>
     )
