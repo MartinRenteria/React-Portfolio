ee014c1995c864e9d475f363efa3d04a04eec151
Martin Renteria <renteriamp96@gmail.com>
2021-04-01 23:08:14 -0700

    Updated Home page by adding background image and new description

--- src/components/AboutMe/About.js
+++ src/components/AboutMe/About.js
@@ -1,30 +1,24 @@
-/* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React from "react";
+import './About.css';
 
 function About() {
-    return (
-    <main className="container">
-    <div id="aboutMe" className="bg-dark p-3">
-      <h3 className="text-light">About Me:</h3>
-      <p className="text-light pl-3 ml-3">
-        Bilingual social services professional looking to expand into the
-        Technology field. Through consistent self-learning and drive to be
-        on top of the latest technologies and skills needed within the
-        industry, I am looking to establish my presence in the ever-growing
-        tech world. Having earned my Bachelors in Psychology and gaining
-        experience within the field's discipline, I learned interpersonal
-        communication and working in teams that will no doubt assist me in
-        serving others within the world of technology. 
-      </p>
-       <p> Ever since I was young, all I ever wanted to do was to help people.
-        That mindset has not changed. Now, I continue to build my career
-        with the motivation and determination to do just that. By combining
-        my two passions, I believe i can reach and assist individuals using
-        the knowledge I have gained through Technology and Psychology.
-       </p>
-    </div>
-    </main>
-      )
+	return (
+		<main className="container">
+    <h2 className="text-center">Welcome to my Portfolio!</h2>
+			<div id="aboutMe" className="bg-dark p-3">
+				<p className="text-light pl-3 ml-3">
+					Full-stack developer looking for full-time opportunity within web development. Through consistent
+					self-learning and drive to be on top of the latest technologies and skills needed within the
+					industry, I am looking to establish my presence in the ever-growing tech world.
+    </p>
+        <p className="text-light pl-3 ml-3">Ever since I was young, all I ever wanted to do was to help people. That mindset has not changed.
+					Now, I continue to build my career with the motivation and determination to do just that. By
+					combining my two passions, I believe i can reach and assist individuals using the knowledge I have
+					gained through Technology and Psychology.
+        </p>
+			</div>
+		</main>
+	);
 }
 
 export default About;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import About from '../AboutMe/About'
+import About from '../AboutMe/About';
+import './Home.css';
 
 
 
 export default function Home() {
     return (
         <main>
+        <div className="jumbotron jumbotron-fluid d-flex align-items-center">
+            <div className="container text-center">
+            </div>
+        </div>
             <About />
         </main>
-    )
+    );
     
 }
+
