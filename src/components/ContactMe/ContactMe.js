b3eb9bfe0aa67faa832802d1e8b5e96b801759a8
Martin Renteria <renteriamp96@gmail.com>
2021-05-05 11:38:32 -0700

    Adding beginning contact section to portfolio and initials files for skills section

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Tooltip, withStyles } from "@material-ui/core"
+import "./contact.css"
 
-export default function ContactMe() {
-    return <h1>Contact Page</h1>
+const CutomTooltip = withStyles(() => ({
+  tooltip: {
+    fontSize: 16,
+  },
+}))(Tooltip);
+
+export default function Contact() {
+  const [copy, setCopy] = useState("Copy")
+  const emailCopied = () => {
+    navigator.clipboard.writeText("renteriamp96@gmail.com")
+    setCopy("Copied")
+    setTimeout(() => { setCopy("Copy") }, 1000)
+  }
+
+  return (
+    <section class="contact">
+      <p>Please get in touch with me for professional opportunities.</p>
+      <CutomTooltip title={copy} arrow>
+        <button class="email" onClick={emailCopied}>renteriamp96@gmail.com</button>
+      </CutomTooltip>
+    </section>
+
+  )
 }
+
